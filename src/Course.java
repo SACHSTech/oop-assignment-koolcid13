8fee4e6cc6865a97e7828216179e43efe2fbf0b6
koolcid13 <avin.ahmadi23@ycdsbk12.ca>
2022-11-11 14:30:47 -0500

    course class inst. variables added txt file start

---
+++ src/Course.java
@@ -0,0 +1,11 @@
+// package line
+import java.util.ArrayList;
+
+public class Course {
+    
+    private String strName;
+    private ArrayList<String> prereqs;
+    private int intEnrolledCnt;
+
+    
+}
